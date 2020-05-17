a8d65d2fdc6ec88a82377198432c5ab48ee48d20
Daniel DANG <quandangnhu@gmail.com>
2020-05-17 08:52:40 +0200

    update

--- cmd/easyscan/easyscan.go
+++ cmd/easyscan/easyscan.go
@@ -16,87 +16,87 @@ const maxUploadSize = 2 * 1024 * 1024 // 2MB
 const uploadPath = "/tmp"
 
 func main() {
-  http.HandleFunc("/upload", uploadFileHandler())
+	http.HandleFunc("/upload", uploadFileHandler())
 
-  fs := http.FileServer(http.Dir(uploadPath))
-  http.Handle("/files/", http.StripPrefix("/files",fs))
+	fs := http.FileServer(http.Dir(uploadPath))
+	http.Handle("/files/", http.StripPrefix("/files", fs))
 
-  log.Print("Server started on localhost:8080")
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Print("Server started on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func uploadFileHandler() http.HandlerFunc {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    
-    if r.Method == "GET" {
-      t, _ := template.ParseFiles("upload.gtpl")
-      t.Execute(w, nil)
-      return
-    }
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-    if err:= r.ParseMultipartForm(maxUploadSize); err != nil {
-      fmt.Printf("Could not parse multipart form: %v\n", err)
-      renderError(w, "CANT_PARSE_FORM", http.StatusBadRequest)
-    }
-    
-    file, fileheader, err := r.FormFile("uploadFile")
-    if err != nil {
-      renderError(w, "INVALID_FILE", http.StatusBadRequest)
-      return
-    }
-    defer file.Close()
+		if r.Method == "GET" {
+			t, _ := template.ParseFiles("upload.gtpl")
+			t.Execute(w, nil)
+			return
+		}
 
-    fileSize := fileheader.Size
-    fmt.Printf("File size (bytes): %v\n", fileSize)
-    if fileSize > maxUploadSize {
-      renderError(w, "FILE_TO_BIG", http.StatusBadRequest)
-      return
-    }
+		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
+			fmt.Printf("Could not parse multipart form: %v\n", err)
+			renderError(w, "CANT_PARSE_FORM", http.StatusBadRequest)
+		}
 
-    fileBytes, err := ioutil.ReadAll(file)
-    if err != nil {
-      renderError(w, "INVALID_FILE", http.StatusBadRequest)
-      return
-    }
+		file, fileheader, err := r.FormFile("uploadFile")
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
 
-    filetype := http.DetectContentType(fileBytes)
-    if filetype != "image/jpeg" && filetype != "image/jpg" &&
-        filetype != "image/gif" && filetype != "image/png" && 
-        filetype != "application/pdf" {
-      renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
-      return
-    }
+		fileSize := fileheader.Size
+		fmt.Printf("File size (bytes): %v\n", fileSize)
+		if fileSize > maxUploadSize {
+			renderError(w, "FILE_TO_BIG", http.StatusBadRequest)
+			return
+		}
 
-    fileName := randToken(12)
-    fileEndings, err := mime.ExtensionsByType(filetype)
-    if err != nil {
-      renderError(w, "CANT_READ_FILE_TYPE", http.StatusBadRequest)
-      return
-    }
-    newPath := filepath.Join(uploadPath, fileName + fileEndings[0])
-    fmt.Printf("FileType: %s, File: %s \n", filetype, newPath)
+		fileBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
 
-    newFile, err := os.Create(newPath)
-    if err != nil {
-      renderError(w, "CANT_CREATE_FILE_ON_DISK:" + err.Error(), http.StatusInternalServerError)
-      return
-    }
-    defer newFile.Close()
-    if _, err := newFile.Write(fileBytes); err != nil {
-      renderError(w, "CANT_WRITE_FILE" + err.Error(), http.StatusInternalServerError)
-    }
-    w.Write([]byte("SUCCESS"))
-  })
+		filetype := http.DetectContentType(fileBytes)
+		if filetype != "image/jpeg" && filetype != "image/jpg" &&
+			filetype != "image/gif" && filetype != "image/png" &&
+			filetype != "application/pdf" {
+			renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
+			return
+		}
+
+		fileName := randToken(12)
+		fileEndings, err := mime.ExtensionsByType(filetype)
+		if err != nil {
+			renderError(w, "CANT_READ_FILE_TYPE", http.StatusBadRequest)
+			return
+		}
+		newPath := filepath.Join(uploadPath, fileName+fileEndings[0])
+		fmt.Printf("FileType: %s, File: %s \n", filetype, newPath)
+
+		newFile, err := os.Create(newPath)
+		if err != nil {
+			renderError(w, "CANT_CREATE_FILE_ON_DISK:"+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer newFile.Close()
+		if _, err := newFile.Write(fileBytes); err != nil {
+			renderError(w, "CANT_WRITE_FILE"+err.Error(), http.StatusInternalServerError)
+		}
+		w.Write([]byte("SUCCESS"))
+	})
 }
 
 func renderError(w http.ResponseWriter, message string, statusCode int) {
-  w.WriteHeader(statusCode)
-  w.Write([]byte(message))
+	w.WriteHeader(statusCode)
+	w.Write([]byte(message))
 }
 
 func randToken(l int) string {
-  b := make([]byte, l)
-  rand.Read(b)
-  return fmt.Sprintf("%x", b)
+	b := make([]byte, l)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
 
 }
