025300df681705e17f861f1210ce7d0d9414f924
Daniel DANG <quandangnhu@gmail.com>
2020-05-23 19:51:14 +0200

    add Crud documents

--- cmd/easyscan/main.go
+++ cmd/easyscan/main.go
@@ -2,37 +2,80 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 // Document
 type Document struct {
+	ID           int    `json:"id", xml:"id"`
 	Name         string `json:"name" xml:"name"`
 	Type         string `json:"type" xml:"type"`
-	Size         int64  `json:"size" xml:"size"`
+	Size         int    `json:"size" xml:"size"`
 	CreationDate string `json:"creationDate" xml:"creationDate"`
 }
 
+var (
+	docs = map[int]*Document{}
+	seq  = 1
+)
+
+//----------
 // Handler
-func returnJson(c echo.Context) error {
-	doc := &Document{
-		Name:         "test",
-		Type:         "Facture",
-		Size:         1000,
-		CreationDate: "2020",
+//----------
+
+func createDocument(c echo.Context) error {
+	d := &Document{
+		ID: seq,
 	}
-	return c.JSONPretty(http.StatusOK, doc, " ")
+	if err := c.Bind(d); err != nil {
+		return err
+	}
+	docs[d.ID] = d
+	seq++
+	return c.JSONPretty(http.StatusCreated, d, " ")
+}
+
+func getDocument(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSONPretty(http.StatusOK, docs[id], " ")
+}
+
+func getAllDocuments(c echo.Context) error {
+	return c.JSONPretty(http.StatusOK, docs, " ")
+}
+
+func updateDocument(c echo.Context) error {
+	d := new(Document)
+	if err := c.Bind(d); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	docs[id].Name = d.Name
+	return c.JSONPretty(http.StatusOK, docs[id], " ")
+}
+
+func deleteDocument(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(docs, id)
+	return c.NoContent(http.StatusNoContent)
 }
 
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello world")
-	})
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	e.GET("/docs", returnJson)
+	// Routes
+	e.GET("/docs", getAllDocuments)
+	e.POST("/docs", createDocument)
+	e.GET("/docs/:id", getDocument)
+	e.PUT("/docs/:id", updateDocument)
+	e.DELETE("/docs/:id", deleteDocument)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module misoda.fr/easyscan
 go 1.14
 
 require (
-	github.com/labstack/echo/v4 v4.1.16
+	github.com/labstack/echo/v4 v4.1.6
+
 	github.com/stretchr/objx v0.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
 	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2 // indirect
