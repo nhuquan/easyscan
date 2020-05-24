8ae6b22be172c072b981918ee50d3c0c8e7effba
Daniel DANG <quandangnhu@gmail.com>
2020-05-24 20:08:12 +0200

    update license

--- pkg/db/repo.go
+++ pkg/db/repo.go
@@ -1,30 +1,62 @@
 package db
 
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"time"
+)
+
 type Repo interface {
-	getOne(id string) (interface{}, error)
-	getAll() ([]interface{}, error)
-	addOne(item interface{}) (interface{}, error)
-	update(id string, item interface{}) (interface{}, error)
+	GetOne(id string) (interface{}, error)
+	GetAll() ([]interface{}, error)
+	AddOne(item interface{}) (interface{}, error)
+	Update(id string, item interface{}) (interface{}, error)
 }
 
 type Doc struct{}
 
-func (d Doc) getOne(id string) (interface{}, error) {
+func (d Doc) GetOne(id string) (interface{}, error) {
+	// TODO
+	db, err := sql.Open("sqlite3", "./easyscan_2020_05.db")
+	if err != nil {
+		return nil, err
+	}
+	q := "Select * from documents where id=?"
+	rows, err := db.Query(q, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var id int
+	var name string
+	var doctype string
+	var creationDate string
+	if rows.Next() {
+		err = rows.Scan(&id, &name, &doctype, &creationDate)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println(id)
+		fmt.Println(name)
+		fmt.Println(doctype)
+		fmt.Println(creationDate)
+	}
+
+	return nil, nil
+}
+
+func (d Doc) GetAll() (interface{}, error) {
 	// TODO
 	return nil, nil
 }
 
-func (d Doc) getAll() (interface{}, error) {
+func (d Doc) AddOne(item interface{}) (interface{}, error) {
 	// TODO
 	return nil, nil
 }
 
-func (d Doc) addOne(item interface{}) (interface{}, error) {
-	// TODO
-	return nil, nil
-}
-
-func (d Doc) update(id string, item interface{}) (interface{}, error) {
+func (d Doc) Update(id string, item interface{}) (interface{}, error) {
 	// TODO
 	return nil, nil
 }
